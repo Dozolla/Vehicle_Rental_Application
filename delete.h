9ef8f2950cbb4dcd06c275bc83583670b9887e3c
lindi24 <aalliu22@epoka.edu.al>
2023-05-30 01:05:02 +0200

    delete a car function implementation

--- delete.h
+++ delete.h
@@ -1,4 +1,20 @@
 #ifndef delete_h
 #define delete_h
 
+void removeVehicle(struct Vehicle **head,int *cnt){
+     struct Vehicle *prev;
+     if (*head==NULL){
+         printf("There are no vehicles to delete\n\n");
+         return;
+     }
+     prev=*head;
+     *head=prev->next;
+     free(prev);
+     printf("Vehicle is deleted.\n\n");
+     int i;
+     i=*cnt;
+     *cnt=i-1;
+
+ }
+ 
 #endif /* delete.h */
